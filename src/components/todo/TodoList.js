982e632c7856f805dc451b6622f223e7d5d776da
Dev-jwJeong <rgn95652@gmail.com>
2022-08-16 00:41:28 +0900

    Feat : 리스트 조회 기능

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoListHook } from '../../hooks/todo/useTodoListHook';
 import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
@@ -9,15 +10,41 @@ const TodoListBlock = styled.div`
   overflow-y: auto;
 `;
 
+const StyledInput = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+  margin-bottom: 10rem;
+`;
+
 function TodoList() {
+  const { todo, onChange, updateText, onToggle, toggle, onGetUpdateTodo } =
+    useTodoListHook();
+
+  console.log(todo);
+
   return (
-    <TodoListBlock>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </TodoListBlock>
+    todo && (
+      <>
+        <TodoListBlock>
+          {todo.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />
+          ))}
+        </TodoListBlock>
+        {toggle && (
+          <>
+            <StyledInput type="text" value={updateText} onChange={onChange} />
+            <button style={{ marginBottom: '10rem' }} onClick={onGetUpdateTodo}>
+              제출
+            </button>
+          </>
+        )}
+      </>
+    )
   );
 }
 
