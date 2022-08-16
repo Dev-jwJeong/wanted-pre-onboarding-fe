4d42c2a323b716984db3bbd045dfdb79f6c98d36
Dev-jwJeong <rgn95652@gmail.com>
2022-08-16 15:10:28 +0900

    Design : 투두리스트 수정 UI

---
+++ src/components/todo/ModifyTodo.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useTodoListHook } from '../../hooks/todo/useTodoListHook';
+
+const ModifyTodoBlock = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const ModifyForm = styled.div`
+  width: 512px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  border-radius: 7px;
+  background-color: lightgray;
+
+  .buttonWrapper {
+    margin-top: 1rem;
+    text-align: center;
+
+    button {
+      padding: 0.5rem;
+    }
+  }
+`;
+
+const StyledInput = styled.input`
+  background: none;
+  outline: none;
+  padding: 0.5rem;
+  font-size: 1.125rem;
+  line-height: 1.5;
+`;
+
+function ModifyTodo({ onToggle, id }) {
+  const { updateText, onChange, onGetUpdateTodo } = useTodoListHook();
+  return (
+    <ModifyTodoBlock>
+      <ModifyForm>
+        <StyledInput type="text" value={updateText} onChange={onChange} />
+        <div className="buttonWrapper">
+          <button onClick={() => onGetUpdateTodo(id)}>수정하기</button>
+          <button style={{ marginLeft: '1rem' }} onClick={onToggle}>
+            취소
+          </button>
+        </div>
+      </ModifyForm>
+    </ModifyTodoBlock>
+  );
+}
+
+export default ModifyTodo;
